4d020e076e54fbf4cfb8d6896a5374ae14e6116f
C-Hajduk <christian.hajduk@outlook.de>
2025-12-08 12:04:36 +0100

    feat: added colorservice & style var colors and radius

---
+++ src/app/services/color-service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class ColorService {
+	profilColors = [
+		'#2563EB',
+		'#9333EA',
+		'#10B981',
+		'#F97316',
+		'#EC4899',
+		'#14B8A6',
+		'#1F2937',
+		'#6B7280',
+		'#F3F4F6',
+		'#FBBF24',
+		'#EF4444',
+		'#06B6D4',
+		'#6366F1',
+		'#84CC16',
+		'#F43F5E'
+	];
+}
