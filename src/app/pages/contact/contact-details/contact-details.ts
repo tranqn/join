85c12a5c5e6973dff8e6102b1aeedfb5d0d34009
C-Hajduk <christian.hajduk@outlook.de>
2025-12-08 18:22:42 +0100

    feat: added deleted & edit button

--- src/app/pages/contact/contact-details/contact-details.ts
+++ src/app/pages/contact/contact-details/contact-details.ts
@@ -1,16 +1,21 @@
-import { Component, input } from '@angular/core';
+import { Component, input, inject } from '@angular/core';
 import { getShortName } from '../contact';
 import { ContactModel } from '../../../interfaces/contact';
+import { FirebaseService } from '../../../services/firebase-service';
+
 
 @Component({
-  selector: 'app-contact-details',
-  imports: [],
-  templateUrl: './contact-details.html',
-  styleUrl: './contact-details.scss',
+	selector: 'app-contact-details',
+	imports: [],
+	templateUrl: './contact-details.html',
+	styleUrl: './contact-details.scss'
 })
 export class ContactDetails {
 	contact = input.required<ContactModel>();
 
+	firebaseService = inject(FirebaseService);
+
+	contacts = this.firebaseService.contacts();
 
 	getInitials(fullName: string) {
 		return getShortName(fullName);
