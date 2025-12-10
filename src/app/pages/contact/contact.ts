6b5886caa98e6e7f886b98d4ac2d15843d2b8435
C-Hajduk <christian.hajduk@outlook.de>
2025-12-10 09:19:47 +0100

    feat: complete sidebar

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -14,6 +14,7 @@ export function getShortName(fullName: string): string {
   styleUrl: './contact.scss',
 })
 export class Contact {
+	isSidebarOpen = signal(false);
 	contactlist = viewChild.required(Contactlist);
 
 	selectedContact = signal<ContactModel | null>(null);
@@ -25,7 +26,11 @@ export class Contact {
 	}
 
 	showContactOptions() {
+		this.isSidebarOpen.update(value => !value);
+	}
 
+	closeSidebar() {
+		this.isSidebarOpen.set(false);
 	}
 
 	onEditContact(contact: ContactModel) {
