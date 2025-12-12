78a412c30bbe84b335837bc58419310ecdad5b56
Daniel Kersten <daniel@dev-kersten.de>
2025-12-12 15:12:02 +0100

    refactor: changed overview to loop functionality

--- src/app/pages/board/task-overview/task-overview.ts
+++ src/app/pages/board/task-overview/task-overview.ts
@@ -1,32 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-task-overview',
-  imports: [],
-  templateUrl: './task-overview.html',
-  styleUrl: './task-overview.scss',
+	selector: 'app-task-overview',
+	imports: [],
+	templateUrl: './task-overview.html',
+	styleUrl: './task-overview.scss'
 })
 export class TaskOverview {
 	columns = [
 		{
-		title: 'To do',
-		id: 'todo',
-		tasks: ['Login Page Design', 'Database Setup']
+			title: 'To do',
+			id: 'todo'
 		},
 		{
-		title: 'In progress',
-		id: 'progress',
-		tasks: ['Contact Form']
+			title: 'In progress',
+			id: 'progress'
 		},
 		{
-		title: 'Await feedback',
-		id: 'feedback',
-		tasks: ['Template Review']
+			title: 'Await feedback',
+			id: 'feedback'
 		},
 		{
-		title: 'Done',
-		id: 'done',
-		tasks: ['CSS Architecture Planning']
+			title: 'Done',
+			id: 'done'
 		}
-  	];
+	];
 }
