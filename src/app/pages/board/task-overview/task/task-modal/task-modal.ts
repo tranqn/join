76ae67c38e28c365d0ca2a4ddf356970ad933326
C-Hajduk <christian.hajduk@outlook.de>
2025-12-19 08:21:20 +0100

    feat: introduce task-modal component to display detailed task information with associated logic and styling.

--- src/app/pages/board/task-overview/task/task-modal/task-modal.ts
+++ src/app/pages/board/task-overview/task/task-modal/task-modal.ts
@@ -1,8 +1,10 @@
-import { Component, input, output, signal } from '@angular/core';
+import { Component, input, output, signal, inject } from '@angular/core';
 import { TaskModel } from '../../../../../interfaces/task';
 import { ContactModel } from '../../../../../interfaces/contact';
 import { Icon } from '../../../../../shared/icon/icon';
 import { DatePipe } from '@angular/common';
+import { Taskservice } from '../../../../../services/taskservice';
+import { getShortName } from '../../../../contact/contact';
 
 @Component({
 	selector: 'app-task-modal',
@@ -11,13 +13,28 @@ import { DatePipe } from '@angular/common';
 	styleUrl: './task-modal.scss'
 })
 export class TaskModal {
+	private taskService = inject(Taskservice);
+
+	assignedContacts = this.taskService.getAssignedContacts(() => this.task());
+	
 	task = input<TaskModel>();
 	editTask = output<ContactModel>();
-
 	close = output<void>();
 	isClosing = signal(false);
 
 	constructor() {}
+
+	getInitials(name: string) {
+		return getShortName(name);
+	}
+
+	getDoneTasks() {
+		const task = this.task();
+		if (!task?.subtasks || !Array.isArray(task.subtasks) || task.subtasks.length === 0) {
+			return 0;
+		}
+		return task.subtasks.filter(subtask => subtask.done).length;
+	}
 
 	closeTaskModal() {
 		this.isClosing.set(true);
